012053bde8c88a68ab4fe3d4a2faad71dbff6e01
cunhang_wei <cunhang_wei@kingdee.com>
2021-07-25 15:39:42 +0800

    24. 两两交换链表中的节点

---
+++ leetcode24.js
@@ -0,0 +1,24 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ * 两两交换链表中的节点
+ */
+var swapPairs = function (head) {
+    let dummy = new ListNode()
+    dummy.next = head
+    let current = dummy
+    
+    while (current.next !== null && current.next.next !== null) {
+        let n1 = current.next
+        let n2 = current.next.next
+        // 开始交换
+        current.next = n2
+        n1.next = n2.next
+        n2.next = n1
+
+        // 继续下一轮的交换
+        current = n1
+    }
+
+    return dummy.next
+};
